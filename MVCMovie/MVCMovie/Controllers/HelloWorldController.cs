33fc1b660dde6c0cdbf1669944fad8c1f769162a
MSI\lpack <l.packster@gmail.com>
2016-09-28 09:14:10 -0600

    init

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //index is called as the default when helloworld is called.
+        //index is defined as the default in the RouteConfig file
+        public ActionResult Index()
+        {
+            //return "This is my <strong>Default</strong> action";
+            return View();  //by default will return a view with the name 
+        }
+        //welcome is pulled up when home/helloWorld/welcome is called
+        //public string Welcome(string name, int id = 1)
+        //{
+        //    //HttpUtility.HtmlEncode not needed for code to work.. it is good practice to keep data more secure
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", Id = " + id);
+        //}
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.numTimes = numTimes;
+            return View();
+        }
+    }
+}
---
+++ MVCMovie/MVCMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;   //must bring this in to use entity framework
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+    //entity framework has a parent class called DbContext which allows us to CRUD foreign key constraints
+    //must add connection string for "MovieDbContext" in webConfig file
+    public class MovieDbContext : DbContext
+    {
+        //DbSet us a collection of object "Movie" that we call "Movies"
+        public DbSet<Movie> Movies { get; set;}
+    }
+}
