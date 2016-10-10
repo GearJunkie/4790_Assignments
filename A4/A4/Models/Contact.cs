6d2914e64cf4d8e1c361d709c728be0bb2729461
loripack1 <loripack1@SC-168-1-PC>
2016-10-10 16:45:11 -0600

    inital

---
+++ A4/A4/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A4.Models
+{
+    public class Contact
+    {
+        public int contactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+
+    }
+}
